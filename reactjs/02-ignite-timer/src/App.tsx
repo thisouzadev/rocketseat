8dfab4e78c3b15247cf67197481336d614008372
thisouzadev <thiagodesouza.dev@gmail.com>
2023-10-05 08:04:01 -0300

    feat: styled Components

---
+++ reactjs/02-ignite-timer/src/App.tsx
@@ -0,0 +1,18 @@
+import { Button } from "./components/Button";
+
+
+export function App() {
+  
+  return (
+    <>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+
+    </>
+  )
+}
+
+
