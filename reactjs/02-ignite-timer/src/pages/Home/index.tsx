e434056f8feeb4cf2fa65966e8d184f38c59a0cb
thisouzadev <thiagodesouza.dev@gmail.com>
2023-10-15 11:35:43 -0300

    Reset do formulário

--- reactjs/02-ignite-timer/src/pages/Home/index.tsx
+++ reactjs/02-ignite-timer/src/pages/Home/index.tsx
@@ -34,13 +34,18 @@ export function Home() {
       minutesAmount: 0,
     },
   })
-  const { handleSubmit, watch /* reset */ } = newCycleForm
+  const { handleSubmit, watch, reset } = newCycleForm
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    createNewCycle(data)
+    reset()
+  }
   const task = watch('task')
   const isSubmitDisable = !task
 
   return (
     <HomeContainer>
-      <form onSubmit={handleSubmit(createNewCycle)}>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormProvider {...newCycleForm}>
           <NewCycleForm />
         </FormProvider>
