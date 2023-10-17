20da0ed34abf77efd0c4768a1f2b5d953bc95fea
thisouzadev <thiagodesouza.dev@gmail.com>
2023-10-17 14:07:40 -0300

    Filtrando lista do banco de dados

--- nodejs/01-fundamentos-nodejs/src/database.js
+++ nodejs/01-fundamentos-nodejs/src/database.js
@@ -19,8 +19,16 @@ export class Database {
     fs.writeFile(databasePath, JSON.stringify(this.#database))
   }
 
-  select(table) {
-    const data = this.#database[table] ?? []
+  select(table, search) {
+    let data = this.#database[table] ?? []
+
+    if (search) {
+      data = data.filter(row => {
+        return Object.entries(search).some(([key, value]) => {
+          return row[key].toLowerCase().includes(value.toLowerCase())
+        })
+      })
+    }
 
     return data
   }
@@ -46,7 +54,7 @@ export class Database {
       this.#persist()
     }
   }
-  
+
   delete(table, id) {
     const rowIndex = this.#database[table].findIndex(row => row.id === id)
 
