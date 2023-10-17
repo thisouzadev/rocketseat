e84eeeaad8ab4eb2e10f12a653eae6d840c9965b
thisouzadev <thiagodesouza.dev@gmail.com>
2023-10-17 13:15:19 -0300

    Rotas da aplicação - Separando rotas da aplicação

--- nodejs/01-fundamentos-nodejs/src/server.js
+++ nodejs/01-fundamentos-nodejs/src/server.js
@@ -1,7 +1,7 @@
 import http from 'node:http'
 import { json } from './middlewares/json.js'
+import { routes } from './routes.js'
 import { Database } from './database.js'
-import { randomUUID } from 'node:crypto'
 
 const database = new Database()
 
@@ -10,25 +10,14 @@ const server = http.createServer(async (req, res) => {
 
   await json(req, res)
 
-  if (method === 'GET' && url === '/users') {
-    const users = database.select('users')
-
-    return res.end(JSON.stringify(users))
+  const route = routes.find(route => {
+    return route.method === method && route.path === url
+  })
+  
+  if (route) {
+    return route.handler(req, res)
   }
 
-    const { name, email } = req.body
-
-  if (method === 'POST' && url === '/users') {
-    const user = {
-      id: randomUUID(),
-      name,
-      email,
-    }
-
-    database.insert('users', user)
-
-    return res.writeHead(201).end()
-  }
 
   return res.end('Hello World')
 })
