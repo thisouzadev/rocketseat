88533984484ed663ac896488f11b0c51fb243c7a
thisouzadev <thiagodesouza.dev@gmail.com>
2023-10-17 12:27:49 -0300

    Consumindo uma stream completa

--- nodejs/01-fundamentos-nodejs/streams/fake-upload-to-http-stream.js
+++ nodejs/01-fundamentos-nodejs/streams/fake-upload-to-http-stream.js
@@ -21,4 +21,8 @@ class OneToHundredStream extends Readable {
 fetch('http://localhost:3334', {
   method: 'POST',
   body: new OneToHundredStream(),
+}).then(res => {
+  return res.text()
+}).then(data => {
+  console.log(data)
 })
