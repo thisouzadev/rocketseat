ab9bb5f2d85b4cd8154f289ef4b152e4e48e4838
thisouzadev <thiagodesouza.dev@gmail.com>
2023-10-17 13:10:40 -0300

    Criando ID único  Persistindo banco de dados

--- nodejs/01-fundamentos-nodejs/src/server.js
+++ nodejs/01-fundamentos-nodejs/src/server.js
@@ -1,60 +1,31 @@
 import http from 'node:http'
+import { json } from './middlewares/json.js'
+import { Database } from './database.js'
+import { randomUUID } from 'node:crypto'
 
-// - Criar usuários
-// - Listagem usuários
-// - Edição de usuários
-// - Remoção de usuários
-
-// - HTTP
-//   - Método HTTP
-//   - URL
-
-// GET, POST, PUT, PATCH, DELETE
-
-// GET => Buscar um recurso do back-end
-// POST => Criar um recurso no back-end
-// PUT => Atualizar um recurso no back-end
-// PATCH => Atualizar uma informação específica de um recurso no back-end
-// DELETE => Deletar um recurso do back-end
-
-// GET /users => Buscando usuários no banc-end
-// POST /users => Criar um usuário no back-end
-// Stateful - Stateless
-
-// Cabeçalhos (Requisição/resposta) => Metadados
-
-const users = []
+const database = new Database()
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
-  const buffers = []
-
-  for await (const chunk of req) {
-    buffers.push(chunk)
-  }
-
-  try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString())
-  } catch {
-    req.body = null
-  }
-
+  await json(req, res)
 
   if (method === 'GET' && url === '/users') {
-    return res
-    .setHeader('Content-type', 'application/json')
-    .end(JSON.stringify(users))
+    const users = database.select('users')
+
+    return res.end(JSON.stringify(users))
   }
 
     const { name, email } = req.body
 
   if (method === 'POST' && url === '/users') {
-    users.push({
-      id: 1,
+    const user = {
+      id: randomUUID(),
       name,
       email,
-    })
+    }
+
+    database.insert('users', user)
 
     return res.writeHead(201).end()
   }
