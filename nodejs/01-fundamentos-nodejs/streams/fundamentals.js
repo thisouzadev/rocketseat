a200bbb3d376ab18eadee5da638de550af0d0fe8
thisouzadev <thiagodesouza.dev@gmail.com>
2023-10-17 10:01:56 -0300

    Streams no Node.js - Criando stream de leitura

--- nodejs/01-fundamentos-nodejs/streams/fundamentals.js
+++ nodejs/01-fundamentos-nodejs/streams/fundamentals.js
@@ -11,3 +11,23 @@
 // 10mb/s -> 10.000
 
 // Readable Streams / Writable Streams
+import { Readable } from 'node:stream'
+class OneToHundredStream extends Readable {
+  index = 1
+
+_read() {
+  const i = this.index++
+setTimeout(() => {
+  if (i > 100) {
+    this.push(null)
+  } else {
+    const buf = Buffer.from(String(i))
+
+    this.push(buf)
+  }
+}, 1000);
+}
+}
+
+new OneToHundredStream()
+.pipe(process.stdout)
