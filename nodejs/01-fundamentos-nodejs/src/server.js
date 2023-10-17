67a4b5b8b4acf2275cbf268f3c662282432f318a
thisouzadev <thiagodesouza.dev@gmail.com>
2023-10-17 14:05:57 -0300

    Rotas da aplicação - Capturando query parameters

--- nodejs/01-fundamentos-nodejs/src/server.js
+++ nodejs/01-fundamentos-nodejs/src/server.js
@@ -17,7 +17,10 @@ const server = http.createServer(async (req, res) => {
   if (route) {
     const routeParams = req.url.match(route.path)
 
-    req.params = { ...routeParams.groups }
+    const { query, ...params } = routeParams.groups
+
+    req.params = params
+    req.query = query ? extractQueryParams(query) : {}
 
     return route.handler(req, res)
   }
