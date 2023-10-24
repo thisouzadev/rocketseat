1814f80dcb3c84f64f7ae4cb014c3955a790c362
thisouzadev <thiagodesouza.dev@gmail.com>
2023-10-24 20:26:15 -0300

    Implementando rotas - Listagem das transações

--- nodejs/02-api-res-nodejs/src/routes/transactions.ts
+++ nodejs/02-api-res-nodejs/src/routes/transactions.ts
@@ -4,12 +4,33 @@ import { z } from 'zod'
 import { randomUUID } from 'node:crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+
+    return { transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionsParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionsParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return {
+      transaction,
+    }
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
       amount: z.number(),
       type: z.enum(['credit', 'debit']),
     })
+    console.log(oi)
 
     const { title, amount, type } = createTransactionBodySchema.parse(
       request.body,
--- nodejs/02-api-res-nodejs/src/server.ts
+++ nodejs/02-api-res-nodejs/src/server.ts
@@ -13,5 +13,5 @@ app
     port: env.PORT,
   })
   .then(() => {
-    console.log('HTTP Server Running!')
+    console.log('HTTP Server Running!', env.PORT)
   })
