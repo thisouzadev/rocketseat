fa0259c1fb37d8d6eba79b2799db12fc59e6ffa5
thisouzadev <thiagodesouza.dev@gmail.com>
2023-10-29 19:38:27 -0300

    FunctionalErrorHandling - Refatorando casos de uso

--- nodejs/04-ignite-nodejs-clean-domain/src/domain/forum/application/use-cases/get-question-by-slug.ts
+++ nodejs/04-ignite-nodejs-clean-domain/src/domain/forum/application/use-cases/get-question-by-slug.ts
@@ -1,13 +1,18 @@
 import { Question } from '@/domain/forum/enterprise/entities/question'
 import { QuestionsRepository } from '../repositories/questions-repository'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { Either, left, right } from '@/core/either'
 
 interface GetQuestionBySlugUseCaseRequest {
   slug: string
 }
 
-interface GetQuestionBySlugUseCaseResponse {
-  question: Question
-}
+type GetQuestionBySlugUseCaseResponse = Either<
+  ResourceNotFoundError,
+  {
+    question: Question
+  }
+>
 
 export class GetQuestionBySlugUseCase {
   constructor(private questionsRepository: QuestionsRepository) {}
@@ -18,11 +23,11 @@ export class GetQuestionBySlugUseCase {
     const question = await this.questionsRepository.findBySlug(slug)
 
     if (!question) {
-      throw new Error('Question not found.')
+      return left(new ResourceNotFoundError())
     }
 
-    return {
+    return right({
       question,
-    }
+    })
   }
 }
