47bd92be0a524ad656b73c3c858477be6dfaab02
thisouzadev <thiagodesouza.dev@gmail.com>
2024-05-02 10:37:24 -0300

    Conexão com a API - Criando contexto de transações

--- reactjs/03-dt-money/src/App.tsx
+++ reactjs/03-dt-money/src/App.tsx
@@ -2,14 +2,17 @@ import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 import { Transactions } from "./pages/Transactions";
+import { TransactionsProvider } from "./contexts/TransactionsContext";
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
     <GlobalStyle />
-    <Transactions />
- </ThemeProvider>
+    <TransactionsProvider>
+        <Transactions />
+      </TransactionsProvider>
+    </ThemeProvider>
   )
 }
 
