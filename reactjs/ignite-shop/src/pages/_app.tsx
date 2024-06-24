5cea2a3676a6e20bb8f07b3d5f46a62dd8baa44e
thisouza <thiagodesouza.dev@gmail.com>
2024-06-24 17:15:14 -0300

    Estrutura visual - Estilos globais

--- reactjs/ignite-shop/src/pages/_app.tsx
+++ reactjs/ignite-shop/src/pages/_app.tsx
@@ -1,5 +1,10 @@
-import type { AppProps } from "next/app";
+import { AppProps } from "next/app"
+import { globalStyles } from "../styles/global"
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+globalStyles()
+
+function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
+
+export default App
