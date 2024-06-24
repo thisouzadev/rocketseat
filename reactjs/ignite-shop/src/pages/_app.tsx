e12b157aa71bfbefd20440bb2416b9f8c625fb7c
thisouza <thiagodesouza.dev@gmail.com>
2024-06-24 17:39:28 -0300

    Estrutura visual - Imagens no Next.js

--- reactjs/ignite-shop/src/pages/_app.tsx
+++ reactjs/ignite-shop/src/pages/_app.tsx
@@ -2,13 +2,15 @@ import { AppProps } from "next/app"
 import { globalStyles } from "../styles/global"
 import logoImg from "../assets/logo.svg"
 import { Container, Header } from "../styles/pages/app"
+import Image from 'next/image'
+
 globalStyles()
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+      <Image src={logoImg} alt="" />
       </Header>
 
       <Component {...pageProps} />
