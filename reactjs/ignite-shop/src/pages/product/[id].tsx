d370aefab3c177009c10b6949a0e32a64050825e
thisouza <thiagodesouza.dev@gmail.com>
2024-06-24 21:29:35 -0300

    Produto & Checkout - SSG com parâmetro dinâmico

--- reactjs/ignite-shop/src/pages/product/[id].tsx
+++ reactjs/ignite-shop/src/pages/product/[id].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from "next"
+import { GetStaticPaths, GetStaticProps } from "next"
 import Image from 'next/image'
 import Stripe from "stripe";
 import { stripe } from "../../lib/stripe";
@@ -35,6 +35,16 @@ export default function Product({ product }: ProductProps) {
   )
 }
 
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      {
+        params: { id: 'prod_MLH5Wy0Y97hDAC' }
+      }
+    ],
+    fallback: false,
+  }
+}
 
 export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
   const productId = params!.id;
